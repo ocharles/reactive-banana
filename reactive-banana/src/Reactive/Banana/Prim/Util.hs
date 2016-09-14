fe56ee099bef45fffb83bc068c62167440dc173b
Ollie Charles <ollie@ocharles.org.uk>
2016-09-14 22:44:16 +0100

    INLINE Prim.Util

--- reactive-banana/src/Reactive/Banana/Prim/Util.hs
+++ reactive-banana/src/Reactive/Banana/Prim/Util.hs
@@ -20,32 +20,41 @@ import           System.Mem.Weak
 debug :: MonadIO m => String -> m ()
 -- debug = liftIO . putStrLn
 debug _ = return ()
+{-# INLINE debug #-}
 
 nop :: Monad m => m ()
 nop = return ()
+{-# INLINE nop #-}
 
 {-----------------------------------------------------------------------------
     IORefs that can be hashed
 ------------------------------------------------------------------------------}
 data Ref a = Ref !(IORef a) !Unique
 
-instance Hashable (Ref a) where hashWithSalt s (Ref _ u) = hashWithSalt s u 
+instance Hashable (Ref a) where
+  hashWithSalt s (Ref _ u) = hashWithSalt s u
+  {-# INLINE hashWithSalt #-}
 
 equalRef :: Ref a -> Ref b -> Bool
 equalRef (Ref _ a) (Ref _ b) = a == b
+{-# INLINE equalRef #-}
 
 newRef :: MonadIO m => a -> m (Ref a)
 newRef a = liftIO $ liftM2 Ref (newIORef a) newUnique
+{-# INLINE newRef #-}
 
 readRef :: MonadIO m => Ref a -> m a
 readRef ~(Ref ref _) = liftIO $ readIORef ref
+{-# INLINE readRef #-}
 
 put :: MonadIO m => Ref a -> a -> m ()
 put ~(Ref ref _) = liftIO . writeIORef ref
+{-# INLINE put #-}
 
 -- | Strictly modify an 'IORef'.
 modify' :: MonadIO m => Ref a -> (a -> a) -> m ()
 modify' ~(Ref ref _) f = liftIO $ readIORef ref >>= \x -> writeIORef ref $! f x
+{-# INLINE modify' #-}
 
 {-----------------------------------------------------------------------------
     Weak pointers
@@ -58,13 +67,17 @@ mkWeakIORefValueFinalizer r@(GHC.IORef (GHC.STRef r#)) v (GHC.IO f) = GHC.IO $ \
 mkWeakIORefValueFinalizer r@(GHC.IORef (GHC.STRef r#)) v f = GHC.IO $ \s ->
   case GHC.mkWeak# r# v f s of (# s1, w #) -> (# s1, GHC.Weak w #)
 #endif
+{-# INLINE mkWeakIORefValueFinalizer #-}
 
 mkWeakIORefValue :: IORef a -> value -> IO (Weak value)
 mkWeakIORefValue a b = mkWeakIORefValueFinalizer a b (return ())
+{-# INLINE mkWeakIORefValue #-}
 
 mkWeakRefValue :: MonadIO m => Ref a -> value -> m (Weak value)
 mkWeakRefValue (Ref ref _) v = liftIO $ mkWeakIORefValue ref v
+{-# INLINE mkWeakRefValue #-}
 
 -- | Dereference a list of weak pointers while discarding dead ones.
 deRefWeaks :: [Weak v] -> IO [v]
 deRefWeaks ws = {-# SCC deRefWeaks #-} fmap catMaybes $ mapM deRefWeak ws
+{-# INLINE deRefWeaks #-}
